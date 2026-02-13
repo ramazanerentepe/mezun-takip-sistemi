20ac7a28fa57b31d5a7433daa560e96840fe8484
ramazan_eren_tepe <teperamazaneren@gmail.com>
2026-02-13 15:34:01 +0300

    feed yapısı temel iskelet

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
-// Font ayarı (Next.js varsayılan fontu)
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  // Varsayılan başlık ve Şablon (Örn: Giriş Yap | Mezun Takip Sistemi)
-  title: {
-    default: 'Mezun Takip Sistemi',
-    template: '%s | Mezun Takip Sistemi',
-  },
-  description: 'Mezun ve Akademisyen Takip Portalı',
-  // Tarayıcı sekmesindeki ikon (Favicon)
-  icons: {
-    icon: '/logo.png', // Public klasörüne bu isimde bir resim koymalısın
-    apple: '/logo.png',
-  },
-}
+  title: "Mezun Takip Sistemi",
+  description: "KTÜN Mezun Takip ve İletişim Portalı",
+};
 
 export default function RootLayout({ children }) {
   return (
-    // 1. HTML dilini Türkçe yapıyoruz (Türkçe karakter sorunlarını ve çeviri uyarısını çözer)
-    <html lang="tr">
-      <body className={inter.className}>{children}</body>
+    <html lang="tr" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}> {/*defaultTheme="system" yap*/} 
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
